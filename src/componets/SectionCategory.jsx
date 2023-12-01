33515bb6a3b27eaf40b0637b6b63de40f48f3a01
Alex Chipre <alechipre@gmail.com>
2023-12-01 18:18:08 -0500

    Add interaction in the input Category

--- src/componets/SectionCategory.jsx
+++ src/componets/SectionCategory.jsx
@@ -3,6 +3,23 @@ import './sectionCategory.css'
 import mokupCategory from '../assets/mokupsCategory.json'
 import { FolderCategory, IconFilter, IconSend } from './Icons'
 export default function SectionCategory () {
+  // Agregar Category
+  const [categories, setCategories] = useState(mokupCategory)
+  const [category, setCategory] = useState('')
+
+  const pushCategory = () => {
+    const fechaActual = new Date()
+    const newCategory = {
+      id: Math.random().toString(),
+      nombre: category,
+      date: fechaActual.toUTCString()
+    }
+    setCategories([...categories, newCategory])
+    setCategory('')
+    setVisibleInputCategory(false)
+  }
+
+  // interaccion en el Input Category
   const [visibleInputCategory, setVisibleInputCategory] = useState(false)
   const refInputCategory = useRef(null)
   const addCategory = () => {
@@ -15,7 +32,7 @@ export default function SectionCategory () {
     <section className="sectionCategories">
       <IconFilter />
       <div className="categories">
-        {mokupCategory.map(category => (
+        {categories.map(category => (
           <p className="category" key={category.id}>
             {category.nombre}
           </p>
@@ -23,17 +40,17 @@ export default function SectionCategory () {
       </div>
       <FolderCategory addCategory={addCategory} />
       {visibleInputCategory && (
-        <div className='sectionInputCategory'>
-
+        <div className="sectionInputCategory">
           <input
             className="inputCategory"
             type="text"
             placeholder="Agregar Category"
             ref={refInputCategory}
+            value={category}
+            onChange={e => setCategory(e.target.value)}
           />
-          <IconSend />
+          <IconSend pushCategory={pushCategory} />
         </div>
-
       )}
     </section>
   )
