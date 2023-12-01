790a0397fa878779e9dc8e69770a87a3e1fc7320
Alex Chipre <alechipre@gmail.com>
2023-12-01 16:38:38 -0500

    Add input category

--- src/componets/SectionCategory.jsx
+++ src/componets/SectionCategory.jsx
@@ -1,7 +1,16 @@
+import { useState, useRef } from 'react'
 import './sectionCategory.css'
 import mokupCategory from '../assets/mokupsCategory.json'
-import IconFilter, { FolderCategory } from './Icons'
+import { FolderCategory, IconFilter, IconSend } from './Icons'
 export default function SectionCategory () {
+  const [visibleInputCategory, setVisibleInputCategory] = useState(false)
+  const refInputCategory = useRef(null)
+  const addCategory = () => {
+    setVisibleInputCategory(!visibleInputCategory)
+    setTimeout(() => {
+      refInputCategory?.current?.focus()
+    }, 1)
+  }
   return (
     <section className="sectionCategories">
       <IconFilter />
@@ -12,8 +21,20 @@ export default function SectionCategory () {
           </p>
         ))}
       </div>
-      <FolderCategory />
+      <FolderCategory addCategory={addCategory} />
+      {visibleInputCategory && (
+        <div className='sectionInputCategory'>
 
+          <input
+            className="inputCategory"
+            type="text"
+            placeholder="Agregar Category"
+            ref={refInputCategory}
+          />
+          <IconSend />
+        </div>
+
+      )}
     </section>
   )
 }
