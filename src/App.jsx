c73421ca79b15d3687802a3868e13fa79ac5932a
Alex Chipre <alechipre@gmail.com>
2023-12-06 16:59:05 -0500

    Add Edit Part

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 import { IconAdd, IconDelete, IconNotSearch } from './componets/Icons'
 
 export default function App () {
-  const { todos, removeTodo } = useStore()
+  const { todos, addTodo, removeTodo } = useStore()
   const localStorageTodos =
     localStorage.getItem('todos') === null
       ? localStorage.setItem('todos', JSON.stringify(todos))
@@ -26,15 +26,19 @@ export default function App () {
     setIsOpenModalTodo(true)
   }
   const closeModal = () => {
+    refNota.current.classList.add('notaClose')
+
     setTimeout(() => {
       setIsOpenModalTodo(false)
+      refNota.current.classList.remove('notaClose')
     }, 250)
   }
 
-  const deleteTodo = id => {
+  const deleteTodo = (id, e) => {
+    e.stopPropagation()
     const localStorageTodos = JSON.parse(localStorage.getItem('todos'))
 
-    const newTodo = (localStorageTodos.filter(todo => todo.id !== id))
+    const newTodo = localStorageTodos.filter(todo => todo.id !== id)
     removeTodo(id)
     localStorage.setItem('todos', JSON.stringify(newTodo))
   }
@@ -59,9 +63,9 @@ export default function App () {
   const filtercategoryName = filterbycategory?.at(0)?.nombre
 
   const todosFilter =
-  filtercategoryName === 'Todos'
-    ? localStorageTodos
-    : localStorageTodos?.filter(todo => todo.category === filtercategoryName)
+    filtercategoryName === 'Todos'
+      ? localStorageTodos
+      : localStorageTodos?.filter(todo => todo.category === filtercategoryName)
 
   const { valueSearch } = useStoreInputSearch()
   const valueSearchLower = valueSearch.toLowerCase()
@@ -69,35 +73,114 @@ export default function App () {
     todoByCategory =>
       todoByCategory && {
         ...todoByCategory,
-        title: todoByCategory.title.toLowerCase(),
-        infoTodo: todoByCategory.infoTodo.toLowerCase()
+        title: todoByCategory.title,
+        infoTodo: todoByCategory.infoTodo
       }
   )
-  console.log(valueSearchLower.length)
   const newFilterBySearch = arrayTodosLower
     .map(
       arr =>
-        (arr.title.includes(valueSearchLower) || arr.infoTodo.includes(valueSearchLower)) && arr
+        (arr.title.toLowerCase().includes(valueSearchLower) ||
+          arr.infoTodo.toLowerCase().includes(valueSearchLower)) &&
+        arr
     )
     .filter(arry => arry.id)
+
+  // Guardar Save Todo
+  const [todoId, setTodoId] = useState('')
+  const [categoryEdit, setcategoryEdit] = useState('')
+
+  const refNota = useRef()
+  const fechaNewTodo = new Date()
+  const fechaDate = fechaNewTodo.toDateString()
+  const fechaHours = fechaNewTodo.toLocaleTimeString()
+
+  // valor Titulo
+  const [valueInput, setValueInput] = useState('')
+  const newTitle = e => {
+    setValueInput(e.target.value)
+  }
+  // valor Texto
+  const [valueText, setValueText] = useState('')
+  const newText = e => {
+    setValueText(e.target.value)
+  }
+
+  const saveTodo = () => {
+    setTimeout(() => {
+      refNota.current.classList.remove('notaClose')
+      setValueInput('')
+      setValueText('')
+    }, 250)
+
+    const localStorageTodos = JSON.parse(localStorage.getItem('todos'))
+
+    refNota.current.classList.add('notaClose')
+
+    const categoryNow = localStorageCategories.filter(category => category.isSelect === true)[0]
+      .nombre
+
+    const info = {
+      id: Math.random().toString(),
+      title: valueInput,
+      infoTodo: valueText,
+      date: fechaDate,
+      dateHour: fechaHours,
+      category: categoryNow === 'Todos' ? 'Sin Categoria' : categoryNow
+    }
+
+    const infoEdit = {
+      id: todoId,
+      title: valueInput,
+      infoTodo: valueText,
+      date: fechaDate,
+      dateHour: fechaHours,
+      category: categoryEdit
+    }
+    if (todoId !== '') {
+      const indexTodo = localStorageTodos.findIndex(todo => todo.id === todoId)
+      localStorageTodos.splice(indexTodo, 1, infoEdit)
+      addTodo(infoEdit)
+      localStorage.setItem('todos', JSON.stringify(localStorageTodos))
+    } else {
+      const newTodos = [...localStorageTodos, info]
+      addTodo(info)
+      localStorage.setItem('todos', JSON.stringify(newTodos))
+    }
+
+    setTimeout(() => {
+      setIsOpenModalTodo(false)
+    }, 250)
+    setTodoId('')
+  }
+
+  // Editar
+  const handleEditTodo = id => {
+    setIsOpenModalTodo(true)
+    const todoToEdit = JSON.parse(localStorage.getItem('todos')).find(title => title.id === id)
+    setTodoId(id)
+    setValueInput(todoToEdit.title)
+    setValueText(todoToEdit.infoTodo)
+    setcategoryEdit(todoToEdit.category)
+  }
   return (
     <>
+      <LogoReact />
       <main className="principal-main">
         <header className="header">
-          <LogoReact />
           <InputSearch />
           <SectionCategory />
         </header>
         <section className="todos todosWithCloseCategory" ref={refSectionTodo}>
           {newFilterBySearch?.map(todo => (
-            <article className="articleTodo" key={todo.id}>
+            <article onClick={() => handleEditTodo(todo.id)} className="articleTodo" key={todo.id}>
               <p className="articleTitle">{todo.title}</p>
               <p className="articleInfoTodo">{todo.infoTodo}</p>
               <div className="articleFooter">
                 <p className="articleCategory">{todo.category}</p>
                 <p className="articleDate">{todo.date}</p>
               </div>
-              <IconDelete deleteTodo={() => deleteTodo(todo.id)} />
+              <IconDelete deleteTodo={(e) => deleteTodo(todo.id, e)} />
             </article>
           ))}
           {newFilterBySearch?.length === 0 && valueSearchLower.length > 0 && (
@@ -115,7 +198,19 @@ export default function App () {
             </article>
           )}
         </section>
-        {isOpenModalTodo && <Notas closeModal={closeModal} />}
+        {isOpenModalTodo && (
+          <Notas
+            closeModal={closeModal}
+            saveTodo={saveTodo}
+            refNota={refNota}
+            fechaDate={fechaDate}
+            fechaHours={fechaHours}
+            valueInput={valueInput}
+            newTitle={newTitle}
+            valueText={valueText}
+            newText={newText}
+          />
+        )}
       </main>
       <div className="profile">
         <img
--- src/componets/Notas.jsx
+++ src/componets/Notas.jsx
@@ -1,56 +1,19 @@
-import { useRef, useState } from 'react'
 import { IconArrow, IconSave } from './Icons'
 import './nota.css'
-import { useStore, useStoreCategories } from '../store/todoStore'
-export default function Notas ({ closeModal }) {
+export default function Notas ({
+  closeModal,
+  saveTodo,
+  refNota,
+  fechaDate,
+  fechaHours,
+  valueInput,
+  valueText,
+  newTitle,
+  newText
+}) {
   // New Todo
-  const { addTodo } = useStore()
-  const { categories } = useStoreCategories()
-  const localStorageCategories =
-    localStorage.getItem('categories') === null
-      ? localStorage.setItem('categories', JSON.stringify(categories))
-      : JSON.parse(localStorage.getItem('categories'))
-
-  const refNota = useRef()
-
-  const saveTodo = () => {
-    refNota.current.classList.add('notaClose')
-    setTimeout(() => {
-      refNota.current.classList.remove('notaClose')
-    }, 250)
-
-    const localStorageTodos = JSON.parse(localStorage.getItem('todos'))
-
-    const categoryNow = localStorageCategories.filter(category => category.isSelect === true)[0]
-      .nombre
-    const info = {
-      id: Math.random(),
-      title: valueInput,
-      infoTodo: valueText,
-      date: fechaDate,
-      dateHour: fechaHours,
-      category: categoryNow === 'Todos' ? 'Sin Categoria' : categoryNow
-    }
-    const newTodos = [...localStorageTodos, info]
-    addTodo(info)
-    localStorage.setItem('todos', JSON.stringify(newTodos))
-  }
-
-  // valor Titulo
-  const [valueInput, setValueInput] = useState('')
-  const newTitle = e => {
-    setValueInput(e.target.value)
-  }
-  // valor Texto
-  const [valueText, setValueText] = useState('')
-  const newText = e => {
-    setValueText(e.target.value)
-  }
 
   // Valor Fecha
-  const fechaNewTodo = new Date()
-  const fechaDate = fechaNewTodo.toDateString()
-  const fechaHours = fechaNewTodo.toLocaleTimeString()
 
   return (
     <section className="nota" ref={refNota}>
